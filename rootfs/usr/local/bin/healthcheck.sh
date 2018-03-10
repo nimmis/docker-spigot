6b2cedddd864c6beed1c75145a0db7e60a859bff
BuildTools <unconfigured@null.spigotmc.org>
2018-03-10 15:50:16 +0100

    adding healthcheck in Dockerfile

--- rootfs/usr/local/bin/healthcheck.sh
+++ rootfs/usr/local/bin/healthcheck.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# Basic healthcheck for docker, remove # from echo if you want some sort of output
-# during build, it will show as unhealthy in docker ps
+# Basic healthcheck for docker, remove # from echo if you want some sort of output.
+# During build, it will show as unhealthy in docker ps
 
 if /usr/bin/supervisorctl status spigot | grep -q 'RUNNING'; then
  # echo "matched"
